0ad934704617564a7fa3c0ffde8f853cee508c07
Thiago Minguim <Chazy249@gmail.com>
2021-10-15 15:02:33 -0300

    SearchBox

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
 import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
+import { NotificationsNav } from './Header/NotificationsNav'
 import { Profile } from './Header/Profile'
+import { SearchBox } from './Header/SearchBox'
 
 export function Header() {
   return (
@@ -19,41 +21,11 @@ export function Header() {
           .{' '}
         </Text>
       </Text>
-      <Flex
-        as="label"
-        flex="1"
-        py="4"
-        px="8"
-        ml="6"
-        maxWidth={400}
-        alignSelf="center"
-        color="gray.200"
-        position="relative"
-        bg="gray.800"
-        borderRadius="full">
-        <Input
-          color="gray.50"
-          variant="unstyled"
-          px="4"
-          mr="4"
-          placeholder="Buscar na plataforma"
-          _placeholder={{ color: 'gray.400' }}
-        />
 
-        <Icon as={RiSearchLine} fontSize="20" />
-      </Flex>
+      <SearchBox />
+
       <Flex align="center" ml="auto">
-        <HStack
-          spacing="8"
-          mx="8"
-          pr="8"
-          py="1"
-          color="gray.300"
-          borderRightWidth={1}
-          borderColor="gray.700">
-          <Icon as={RiNotificationLine} fontSize="20" />
-          <Icon as={RiUserAddLine} fontSize="20" />
-        </HStack>
+        <NotificationsNav />
 
         <Profile />
       </Flex>
