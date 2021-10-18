ac82c35b505a7574ce6cf780073e862177f894a0
Thiago Minguim <Chazy249@gmail.com>
2021-10-18 15:25:29 -0300

    siderBar Component

--- src/components/Sidebar/Index.tsx
+++ src/components/Sidebar/Index.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Text, Link, Icon } from '@chakra-ui/react'
+import { Box, Stack } from '@chakra-ui/react'
 import {
   RiContactsLine,
   RiDashboardLine,
@@ -6,33 +6,19 @@ import {
   RiInputMethodLine
 } from 'react-icons/ri'
 import { NavSection } from './NavSection'
+import { NavLink } from './NavLink'
 
 export function Sidebar() {
   return (
     <Box as="aside" w="64" mr="8">
       <Stack spacing="12" align="flex-start">
         <NavSection title="GERAL">
-         
-          <Link display="flex" align="center">
-            <Icon as={RiContactsLine} fontSize="20" />
-            <Text ml="4" fontWeight="medium">
-              Usuários
-            </Text>
-          </Link>
+          <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
+          <NavLink icon={RiContactsLine}>Usúarios</NavLink>
         </NavSection>
         <NavSection title="AUTOMAÇÃO">
-          <Link display="flex" align="center">
-            <Icon as={RiInputMethodLine} fontSize="20" />
-            <Text ml="4" fontWeight="medium">
-              Formulários
-            </Text>
-          </Link>
-          <Link display="flex" align="center">
-            <Icon as={RiGitMergeLine} fontSize="20" />
-            <Text ml="4" fontWeight="medium">
-              Automação
-            </Text>
-          </Link>
+          <NavLink icon={RiInputMethodLine}>Formlários</NavLink>
+          <NavLink icon={RiGitMergeLine}>Automação</NavLink>
         </NavSection>
       </Stack>
     </Box>
--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,15 +1,20 @@
-import { Link, Icon, Text } from '@chakra-ui/react'
+import {
+  Link,
+  Icon,
+  Text,
+  LinkProps as ChakraLinkProps
+} from '@chakra-ui/react'
 import { ElementType } from 'react'
 import { RiDashboardLine } from 'react-icons/ri'
 
-interface NavLinkProps {
+interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType
   children: string
 }
 
-export function NavLink({ icon, children }: NavLinkProps) {
+export function NavLink({ icon, children, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" align="center">
+    <Link display="flex" align="center" {...rest}>
       <Icon as={icon} fontSize="20" />
       <Text ml="4" fontWeight="medium">
         {children}
