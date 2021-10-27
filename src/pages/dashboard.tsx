7249f07b63fbbced4e62294606c39b8e03db5503
Thiago Minguim <Chazy249@gmail.com>
2021-10-27 15:28:17 -0300

    Dashboard responsive

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,6 @@
 import { Flex, SimpleGrid, Box, Text, theme } from '@chakra-ui/react'
 import { Header } from '../components/Header/Index'
-import { Sidebar } from '../components/Sidebar'
+import { Sidebar } from '../components/Sidebar/Index'
 import dynamic from 'next/dynamic'
 
 const Chart = dynamic(() => import('react-apexcharts'), {
@@ -66,13 +66,14 @@ export default function Dashboard() {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius="8" pb="4">
+          <Box p={['4', '8']} bg="gray.800" borderRadius="8" pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius="8" pb="4">
+
+          <Box p={['4', '8']} bg="gray.800" borderRadius="8" pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de Abertura
             </Text>
