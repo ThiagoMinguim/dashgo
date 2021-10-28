f4c166016e380d58f956765ddeb14b5f87d401f4
Thiago Minguim <Chazy249@gmail.com>
2021-10-28 09:17:26 -0300

    including next link: index

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,6 +14,7 @@ import {
   Text,
   useBreakpointValue
 } from '@chakra-ui/react'
+import Link from 'next/link'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import { Header } from '../../components/Header/Index'
 import { Pagination } from '../../components/Pagination/Index'
@@ -37,14 +38,17 @@ export default function UserList() {
             <Heading size="lg" fontWeight="normal">
               Usuarios
             </Heading>
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
-              Criar novo
-            </Button>
+
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
           <Table colorScheme="whiteAlpha">
             <Thead>
