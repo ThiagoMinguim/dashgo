9c74b959b66ef5a056c9e91b04b53b410543e80c
Thiago Minguim <Chazy249@gmail.com>
2021-11-03 13:32:57 -0300

    changes

--- src/components/Sidebar/SideBarNav.tsx
+++ src/components/Sidebar/SideBarNav.tsx
@@ -12,12 +12,20 @@ export function SideBarNav() {
   return (
     <Stack spacing="12" align="flex-start">
       <NavSection title="GERAL">
-        <NavLink icon={RiDashboardLine} href="/dashboard">Dashboard</NavLink>
-        <NavLink icon={RiContactsLine} href="/users">Usúarios</NavLink>
+        <NavLink icon={RiDashboardLine} href="/dashboard">
+          Dashboard
+        </NavLink>
+        <NavLink icon={RiContactsLine} href="/users">
+          Usúarios
+        </NavLink>
       </NavSection>
       <NavSection title="AUTOMAÇÃO">
-        <NavLink icon={RiInputMethodLine} href="/forms">Formlários</NavLink>
-        <NavLink icon={RiGitMergeLine} href=".automation">Automação</NavLink>
+        <NavLink icon={RiInputMethodLine} href="/forms">
+          Formlários
+        </NavLink>
+        <NavLink icon={RiGitMergeLine} href="/automation">
+          Automação
+        </NavLink>
       </NavSection>
     </Stack>
   )
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,11 @@
-import { createServer, Model } from 'miragejs'
+import faker from 'faker'
+import {
+  createServer,
+  Factory,
+  Model,
+  Response,
+  ActiveModelSerializer
+} from 'miragejs'
 
 type User = {
   name: string
@@ -6,23 +13,58 @@ type User = {
   created_at: string
 }
 
-export function makeServer() {
-  const server = createServer({
+export const makeServer = () =>
+  createServer({
+    serializers: {
+      application: ActiveModelSerializer
+    },
+
     models: {
       user: Model.extend<Partial<User>>({})
     },
 
+    factories: {
+      user: Factory.extend({
+        name() {
+          return faker.name.findName()
+        },
+        email() {
+          return faker.internet.email().toLowerCase()
+        },
+        createdAt() {
+          return faker.date.recent(10)
+        }
+      })
+    },
+
+    seeds(server) {
+      server.createList('user', 200)
+    },
+
     routes() {
-      this.namespace = ' api'
+      this.namespace = 'api'
       this.timing = 750
 
-      this.get('/users')
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams
+
+        const total = schema.all('user').length
+
+        const pageStart = (Number(page) - 1) * Number(per_page)
+        const pageEnd = pageStart + Number(per_page)
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStart,
+          pageEnd
+        )
+
+        return new Response(200, { 'x-total-count': String(total) }, { users })
+      })
+
+      this.get('/users/:id')
       this.post('/users')
 
       this.namespace = ''
       this.passthrough()
     }
   })
-
-  return server
-}
